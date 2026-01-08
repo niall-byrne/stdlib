9eec6b547486d77dfa1a15db3e9a685eb194d3cc
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-01-08 17:39:05 -0500

    ci(SHELLCHECK): update source paths

--- src/string/lines/__lib__.sh
+++ src/string/lines/__lib__.sh
@@ -4,9 +4,9 @@
 
 builtin set -eo pipefail
 
-# shellcheck source=stdlib/string/lines/join.sh
+# shellcheck source=src/string/lines/join.sh
 source "${STDLIB_DIRECTORY}/string/lines/join.sh"
-# shellcheck source=stdlib/string/lines/map.sh
+# shellcheck source=src/string/lines/map.sh
 source "${STDLIB_DIRECTORY}/string/lines/map.sh"
 
 _STDLIB_DELIMITER=""
--- src/testing/capture/__lib__.sh
+++ src/testing/capture/__lib__.sh
@@ -4,13 +4,13 @@
 
 builtin set -eo pipefail
 
-# shellcheck source=stdlib/testing/capture/assertion_failure.sh
+# shellcheck source=src/testing/capture/assertion_failure.sh
 source "${STDLIB_DIRECTORY}/testing/capture/assertion_failure.sh"
-# shellcheck source=stdlib/testing/capture/output.sh
+# shellcheck source=src/testing/capture/output.sh
 source "${STDLIB_DIRECTORY}/testing/capture/output.sh"
-# shellcheck source=stdlib/testing/capture/rc.sh
+# shellcheck source=src/testing/capture/rc.sh
 source "${STDLIB_DIRECTORY}/testing/capture/rc.sh"
-# shellcheck source=stdlib/testing/capture/stderr.sh
+# shellcheck source=src/testing/capture/stderr.sh
 source "${STDLIB_DIRECTORY}/testing/capture/stderr.sh"
-# shellcheck source=stdlib/testing/capture/stdout.sh
+# shellcheck source=src/testing/capture/stdout.sh
 source "${STDLIB_DIRECTORY}/testing/capture/stdout.sh"
