6c907087b1ee8b7fa2255e7db6cf202cf5c7a4a1
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-01-08 17:39:05 -0500

    ci(SHELLCHECK): update source paths

--- src/array/__lib__.sh
+++ src/array/__lib__.sh
@@ -4,15 +4,15 @@
 
 builtin set -eo pipefail
 
-# shellcheck source=stdlib/array/assert/__lib__.sh
+# shellcheck source=src/array/assert/__lib__.sh
 source "${STDLIB_DIRECTORY}/array/assert/__lib__.sh"
-# shellcheck source=stdlib/array/getter.sh
+# shellcheck source=src/array/getter.sh
 source "${STDLIB_DIRECTORY}/array/getter.sh"
-# shellcheck source=stdlib/array/make.sh
+# shellcheck source=src/array/make.sh
 source "${STDLIB_DIRECTORY}/array/make.sh"
-# shellcheck source=stdlib/array/map.sh
+# shellcheck source=src/array/map.sh
 source "${STDLIB_DIRECTORY}/array/map.sh"
-# shellcheck source=stdlib/array/mutate.sh
+# shellcheck source=src/array/mutate.sh
 source "${STDLIB_DIRECTORY}/array/mutate.sh"
-# shellcheck source=stdlib/array/query/__lib__.sh
+# shellcheck source=src/array/query/__lib__.sh
 source "${STDLIB_DIRECTORY}/array/query/__lib__.sh"
