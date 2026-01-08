d6e1881c7e254e2cdf0bc7b43b6cabb39823356f
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-01-08 17:39:05 -0500

    ci(SHELLCHECK): update source paths

--- src/testing/parametrize/__lib__.sh
+++ src/testing/parametrize/__lib__.sh
@@ -4,17 +4,17 @@
 
 builtin set -eo pipefail
 
-# shellcheck source=stdlib/testing/parametrize/apply.sh
+# shellcheck source=src/testing/parametrize/apply.sh
 source "${STDLIB_DIRECTORY}/testing/parametrize/apply.sh"
-# shellcheck source=stdlib/testing/parametrize/components/configuration.sh
+# shellcheck source=src/testing/parametrize/components/configuration.sh
 source "${STDLIB_DIRECTORY}/testing/parametrize/components/configuration.sh"
-# shellcheck source=stdlib/testing/parametrize/components/create.sh
+# shellcheck source=src/testing/parametrize/components/create.sh
 source "${STDLIB_DIRECTORY}/testing/parametrize/components/create.sh"
-# shellcheck source=stdlib/testing/parametrize/components/debug.sh
+# shellcheck source=src/testing/parametrize/components/debug.sh
 source "${STDLIB_DIRECTORY}/testing/parametrize/components/debug.sh"
-# shellcheck source=stdlib/testing/parametrize/components/validate.sh
+# shellcheck source=src/testing/parametrize/components/validate.sh
 source "${STDLIB_DIRECTORY}/testing/parametrize/components/validate.sh"
-# shellcheck source=stdlib/testing/parametrize/compose.sh
+# shellcheck source=src/testing/parametrize/compose.sh
 source "${STDLIB_DIRECTORY}/testing/parametrize/compose.sh"
-# shellcheck source=stdlib/testing/parametrize/parametrize.sh
+# shellcheck source=src/testing/parametrize/parametrize.sh
 source "${STDLIB_DIRECTORY}/testing/parametrize/parametrize.sh"
