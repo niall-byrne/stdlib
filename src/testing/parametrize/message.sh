0f2163d0c58ae68beb7bc98bb6c52f1821c80cdc
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-01-20 12:47:49 -0500

    feat(GETTEXT): support translations

--- src/testing/parametrize/message.sh
+++ src/testing/parametrize/message.sh
@@ -17,75 +17,75 @@ _testing.parametrize.message.get() {
   case "${key}" in
     PARAMETRIZE_CONFIGURATION_ERROR)
       required_options=0
-      message="Misconfigured parametrize parameters!"
+      message="$(_testing.__gettext "Misconfigured parametrize parameters!")"
       ;;
     PARAMETRIZE_ERROR_DUPLICATE_TEST_VARIANT_DETAIL)
       required_options=0
-      message="This test variant was created twice, please check your parametrize configuration for this test." # noqa
+      message="$(_testing.__gettext "This test variant was created twice, please check your parametrize configuration for this test.")" # noqa
       ;;
     PARAMETRIZE_ERROR_DUPLICATE_TEST_VARIANT_NAME)
       required_options=0
-      message="Duplicate test variant name!" # noqa
+      message="$(_testing.__gettext "Duplicate test variant name!")" # noqa
       ;;
     PARAMETRIZE_ERROR_FN_DOES_NOT_EXIST)
       required_options=0
-      message="It does not exist!"
+      message="$(_testing.__gettext "It does not exist!")"
       ;;
     PARAMETRIZE_ERROR_PARAMETRIZER_FN_INVALID)
       required_options=1
-      message="The function '${option1}' cannot be used in a parametrize series!"
+      message="$(_testing.__gettext "The function '${option1}' cannot be used in a parametrize series!")"
       ;;
     PARAMETRIZE_ERROR_PARAMETRIZER_FN_NAME)
       required_options=0
-      message="It's name must be prefixed with '${_PARAMETRIZE_PARAMETRIZER_PREFIX}' !"
+      message="$(_testing.__gettext "It's name must be prefixed with '${_PARAMETRIZE_PARAMETRIZER_PREFIX}' !")"
       ;;
     PARAMETRIZE_ERROR_TEST_FN_INVALID)
       required_options=1
-      message="The function '${option1}' cannot be parametrized."
+      message="$(_testing.__gettext "The function '${option1}' cannot be parametrized.")"
       ;;
     PARAMETRIZE_ERROR_TEST_FN_NAME)
       required_options=0
-      message="It's name must start with 'test' and contain a '${_PARAMETRIZE_VARIANT_TAG}' tag, please rename this function!"
+      message="$(_testing.__gettext "It's name must start with 'test' and contain a '${_PARAMETRIZE_VARIANT_TAG}' tag, please rename this function!")"
       ;;
     PARAMETRIZE_FOOTER_SCENARIO_VALUES)
       required_options=0
-      message="== End Scenario Values =="
+      message="$(_testing.__gettext "== End Scenario Values ==")"
       ;;
     PARAMETRIZE_HEADER_SCENARIO)
       required_options=0
-      message="Parametrize Scenario"
+      message="$(_testing.__gettext "Parametrize Scenario")"
       ;;
     PARAMETRIZE_HEADER_SCENARIO_VALUES)
       required_options=0
-      message="== Begin Scenario Values =="
+      message="$(_testing.__gettext "== Begin Scenario Values ==")"
       ;;
     PARAMETRIZE_PREFIX_FIXTURE_COMMAND)
       required_options=0
-      message="Fixture Command"
+      message="$(_testing.__gettext "Fixture Command")"
       ;;
     PARAMETRIZE_PREFIX_FIXTURE_COMMANDS)
       required_options=0
-      message="Fixture Commands"
+      message="$(_testing.__gettext "Fixture Commands")"
       ;;
     PARAMETRIZE_PREFIX_SCENARIO_NAME)
       required_options=0
-      message="Scenario Name"
+      message="$(_testing.__gettext "Scenario Name")"
       ;;
     PARAMETRIZE_PREFIX_SCENARIO_VALUES)
       required_options=0
-      message="Value Set"
+      message="$(_testing.__gettext "Value Set")"
       ;;
     PARAMETRIZE_PREFIX_SCENARIO_VARIABLE)
       required_options=0
-      message="Variables"
+      message="$(_testing.__gettext "Variables")"
       ;;
     PARAMETRIZE_PREFIX_TEST_NAME)
       required_options=0
-      message="Test Name"
+      message="$(_testing.__gettext "Test Name")"
       ;;
     PARAMETRIZE_PREFIX_VARIANT_NAME)
       required_options=0
-      message="Variant name"
+      message="$(_testing.__gettext "Variant name")"
       ;;
     "")
       required_options=0
@@ -95,7 +95,7 @@ _testing.parametrize.message.get() {
     *)
       required_options=0
       return_status=126
-      message="Unknown message key '${key}'"
+      message="$(_testing.__gettext "Unknown message key '${key}'")"
       ;;
   esac
 
