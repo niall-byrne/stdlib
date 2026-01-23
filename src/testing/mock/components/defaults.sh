60d3cc2b59fd488ac7daabc7d974c378550741f6
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-01-23 14:39:13 -0500

    refactor(MOCK): simplify compilation

--- src/testing/mock/components/defaults.sh
+++ src/testing/mock/components/defaults.sh
@@ -8,7 +8,7 @@ builtin export __STDLIB_TESTING_MOCK_COMPONENT
 
 # shellcheck disable=SC2034
 __STDLIB_TESTING_MOCK_COMPONENT="$(
-  "${_STDLIB_BINARY_CAT}" << EOF
+  "${_STDLIB_BINARY_CAT}" << 'EOF'
 __${2}_mock_keywords=()
 __${2}_mock_pipeable=0
 __${2}_mock_rc=""
