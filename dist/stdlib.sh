b14269bfaad53f9306565a8a3b6b5c749940e961
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-01-26 17:00:51 -0500

    refactor(FN): standardize fn names

--- dist/stdlib.sh
+++ dist/stdlib.sh
@@ -1856,7 +1856,7 @@ stdlib.setting.colour.enable ()
     builtin local error_message="";
     if ! "${_STDLIB_BINARY_TPUT}" init 2> /dev/null; then
         if [[ "${silent_fallback_boolean}" != "1" ]]; then
-            stdlib.setting.colour.enable._generate_error_message;
+            stdlib.setting.colour.enable.__generate_error_message;
             builtin return 1;
         fi;
         stdlib.setting.colour.disable;
@@ -1866,7 +1866,7 @@ stdlib.setting.colour.enable ()
     stdlib.setting.theme.load
 }
 
-stdlib.setting.colour.enable._generate_error_message ()
+stdlib.setting.colour.enable.__generate_error_message ()
 {
     builtin local error_message="";
     error_message+="$(stdlib.__message.get COLOUR_INITIALIZE_ERROR)\n";
