e4241ec7552fd91b1bbebc9fcbc92633cf209c67
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-01-26 10:28:38 -0500

    feat(ASSERTIONS): add negative assertion for functions

--- src/testing/assertion/fn.sh
+++ src/testing/assertion/fn.sh
@@ -15,3 +15,15 @@ assert_is_fn() {
   _stdlib_assertion_output="${_stdlib_assertion_output/$'\n'/$'\n '}"
   [[ "${_stdlib_return_code}" == "0" ]] || fail " ${_stdlib_assertion_output}"
 }
+
+assert_not_fn() {
+  # $1: the function name to check
+
+  builtin local _stdlib_assertion_output
+  builtin local _stdlib_return_code=0
+
+  _stdlib_assertion_output="$(_testing.__protected stdlib.fn.assert.not_fn "${@}" 2>&1)" || _stdlib_return_code="$?"
+
+  _stdlib_assertion_output="${_stdlib_assertion_output/$'\n'/$'\n '}"
+  [[ "${_stdlib_return_code}" == "0" ]] || fail " ${_stdlib_assertion_output}"
+}
