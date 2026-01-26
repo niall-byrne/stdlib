c49c63e335dff8af37fb7afd9b9c34ec7a65a89f
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-01-26 17:00:51 -0500

    refactor(FN): standardize fn names

--- src/setting/tests/colour/test_stdlib_setting_colour_enable_generate_error_message.sh
+++ src/setting/tests/colour/test_stdlib_setting_colour_enable_generate_error_message.sh
@@ -3,7 +3,7 @@
 test_stdlib_setting_colour_enable_generate_error_message__term_set______generates_expected_stderr() {
   local TERM="xterm"
 
-  _capture.stderr_raw stdlib.setting.colour.enable._generate_error_message
+  _capture.stderr_raw stdlib.setting.colour.enable.__generate_error_message
 
   assert_output "$(stdlib.__message.get COLOUR_INITIALIZE_ERROR)"$'\n'
 }
@@ -11,7 +11,7 @@ test_stdlib_setting_colour_enable_generate_error_message__term_set______generate
 test_stdlib_setting_colour_enable_generate_error_message__term_not_set__generates_expected_stderr() {
   local TERM=""
 
-  _capture.stderr_raw stdlib.setting.colour.enable._generate_error_message
+  _capture.stderr_raw stdlib.setting.colour.enable.__generate_error_message
 
   assert_output "$(stdlib.__message.get COLOUR_INITIALIZE_ERROR)
 $(stdlib.__message.get COLOUR_INITIALIZE_ERROR_TERM)
