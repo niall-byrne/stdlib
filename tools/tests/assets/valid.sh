4ce35e533e7e62e091b43a12dfeb5c44a362eef8
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-01-27 14:07:57 -0500

    docs(SHDOC): document the mock object api

--- tools/tests/assets/valid.sh
+++ tools/tests/assets/valid.sh
@@ -7,3 +7,19 @@
 stdlib.valid() {
   builtin echo "hello"
 }
+
+eval "$(
+  "${_STDLIB_BINARY_CAT}" << 'EOF'
+
+# @description This is a valid function.
+#   * __${2}_mock_global_variable: The greeting message for this function to emit (default="").
+# @arg $1 string A string argument.
+# @exitcode 0 If the operation succeeded.
+# @stdout The configured greeting message.
+${1}.also_valid() {
+
+  builtin echo "__${2}_mock_global_variable"
+}
+
+EOF
+)"
