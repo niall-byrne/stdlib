c0f90ae52454c6815f57e585d2b5c0e969833593
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-04-14 14:33:43 -0400

    docs(SPHINX): translatable documentation

---
+++ docs/conf/html.py
@@ -0,0 +1,8 @@
+"""Sphinx options for HTML output."""
+
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_short_title = ""
+html_static_path = ["_static"]
+html_theme = "sphinx_rtd_theme"
+html_title = ""
