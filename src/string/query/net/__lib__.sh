954d45bb94152ecb2f12297d5790fc24188119a8
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-05-01 16:34:30 -0400

    feat(STRING): functions to validate ip addresses

---
+++ src/string/query/net/__lib__.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# stdlib string query net library
+
+builtin set -eo pipefail
+
+# shellcheck source=src/string/query/net/is.sh
+builtin source "${STDLIB_DIRECTORY}/string/query/net/is.sh"
