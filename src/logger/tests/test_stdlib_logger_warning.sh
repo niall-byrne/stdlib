2b01e368281a2396909005f3c35cf72313eaf722
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-05-06 09:57:42 -0400

    feat(LOGGER): global variable validation

--- src/logger/tests/test_stdlib_logger_warning.sh
+++ src/logger/tests/test_stdlib_logger_warning.sh
@@ -43,3 +43,15 @@ test_stdlib_logger_warning__default_prefix____no_stdout() {
 
   assert_equals "${TEST_EXPECTED}" "${TEST_OUTPUT}"
 }
+
+# shellcheck disable=SC2178
+test_stdlib_logger_warning__invalid_array_____correct_stderr() {
+  local __STDLIB_LOGGING_DECORATORS_ARRAY="invalid_value"
+
+  TEST_EXPECTED="${FUNCNAME[0]}: ${!expected_colour}test string${STDLIB_COLOUR_NC}"$'\n'
+  TEST_INPUT="test string"
+
+  _capture.stderr_raw stdlib.logger.warning "${TEST_INPUT}"
+
+  assert_equals "${TEST_EXPECTED}" "${TEST_OUTPUT}"
+}
