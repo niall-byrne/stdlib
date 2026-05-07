418b3bd7d8d9157100df2bbeb114456e3799b85a
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-05-07 17:11:04 -0400

    feat(SETTING): global variable validation

--- src/setting/tests/colour/test_stdlib_setting_colour_enable.sh
+++ src/setting/tests/colour/test_stdlib_setting_colour_enable.sh
@@ -7,9 +7,26 @@ setup() {
   _mock.create stdlib.setting.colour.disable
 }
 
-test_stdlib_setting_colour_enable__tput_fails_____silent_fallback___does_not_generate_error_message() {
+@parametrize_with_silent_() {
+
+  @parametrize \
+    "${1}" \
+    "SILENT_FALLBACK_BOOLEAN" \
+    "silent_fallback_true___;1"
+}
+
+@parametrize_with_verbose() {
+
+  @parametrize \
+    "${1}" \
+    "SILENT_FALLBACK_BOOLEAN" \
+    "silent_fallback_false__;0" \
+    "silent_fallback_invalid;aaa"
+}
+
+test_stdlib_setting_colour_enable__tput_fails_____@vary__@vary__does_not_generate_error_message() {
   # shellcheck disable=SC2034
-  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="1"
+  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="${SILENT_FALLBACK_BOOLEAN}"
   local TERM=""
 
   stdlib.setting.colour.enable
@@ -17,9 +34,13 @@ test_stdlib_setting_colour_enable__tput_fails_____silent_fallback___does_not_gen
   stdlib.setting.colour.enable.__generate_error_message.mock.assert_not_called
 }
 
-test_stdlib_setting_colour_enable__tput_fails_____silent_fallback___disables_colours() {
+@parametrize.apply \
+  test_stdlib_setting_colour_enable__tput_fails_____@vary__@vary__does_not_generate_error_message \
+  @parametrize_with_silent_
+
+test_stdlib_setting_colour_enable__tput_fails_____@vary__@vary__disables_colours() {
   # shellcheck disable=SC2034
-  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="1"
+  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="${SILENT_FALLBACK_BOOLEAN}"
   local TERM=""
 
   _capture.rc stdlib.setting.colour.enable
@@ -27,9 +48,13 @@ test_stdlib_setting_colour_enable__tput_fails_____silent_fallback___disables_col
   stdlib.setting.colour.disable.mock.assert_called_once_with ""
 }
 
-test_stdlib_setting_colour_enable__tput_fails_____silent_fallback___returns_status_code_0() {
+@parametrize.apply \
+  test_stdlib_setting_colour_enable__tput_fails_____@vary__@vary__disables_colours \
+  @parametrize_with_silent_
+
+test_stdlib_setting_colour_enable__tput_fails_____@vary__@vary__returns_status_code_0() {
   # shellcheck disable=SC2034
-  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="1"
+  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="${SILENT_FALLBACK_BOOLEAN}"
   local TERM=""
 
   _capture.rc stdlib.setting.colour.enable
@@ -37,9 +62,13 @@ test_stdlib_setting_colour_enable__tput_fails_____silent_fallback___returns_stat
   assert_rc "0"
 }
 
-test_stdlib_setting_colour_enable__tput_fails_____silent_fallback___does_not_set_call_colour_values() {
+@parametrize.apply \
+  test_stdlib_setting_colour_enable__tput_fails_____@vary__@vary__returns_status_code_0 \
+  @parametrize_with_silent_
+
+test_stdlib_setting_colour_enable__tput_fails_____@vary__@vary__does_not_set_call_colour_values() {
   # shellcheck disable=SC2034
-  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="1"
+  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="${SILENT_FALLBACK_BOOLEAN}"
   local TERM=""
   local colour
 
@@ -55,9 +84,14 @@ test_stdlib_setting_colour_enable__tput_fails_____silent_fallback___does_not_set
   done
 }
 
-test_stdlib_setting_colour_enable__tput_fails_____verbose_failure___generates_error_message() {
+@parametrize.apply \
+  test_stdlib_setting_colour_enable__tput_fails_____@vary__@vary__does_not_set_call_colour_values \
+  @parametrize_with_verbose \
+  @parametrize_with_silent_
+
+test_stdlib_setting_colour_enable__tput_fails_____@vary__@vary__generates_error_message() {
   # shellcheck disable=SC2034
-  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="0"
+  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="${SILENT_FALLBACK_BOOLEAN}"
   local TERM=""
 
   stdlib.setting.colour.enable
@@ -65,9 +99,13 @@ test_stdlib_setting_colour_enable__tput_fails_____verbose_failure___generates_er
   stdlib.setting.colour.enable.__generate_error_message.mock.assert_called_once_with ""
 }
 
-test_stdlib_setting_colour_enable__tput_fails_____verbose_failure___does_not_disable_colours() {
+@parametrize.apply \
+  test_stdlib_setting_colour_enable__tput_fails_____@vary__@vary__generates_error_message \
+  @parametrize_with_verbose
+
+test_stdlib_setting_colour_enable__tput_fails_____@vary__@vary__does_not_disable_colours() {
   # shellcheck disable=SC2034
-  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="0"
+  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="${SILENT_FALLBACK_BOOLEAN}"
   local TERM=""
 
   _capture.rc stdlib.setting.colour.enable
@@ -75,9 +113,13 @@ test_stdlib_setting_colour_enable__tput_fails_____verbose_failure___does_not_dis
   stdlib.setting.colour.disable.mock.assert_not_called
 }
 
-test_stdlib_setting_colour_enable__tput_fails_____verbose_failure___returns_status_code_127() {
+@parametrize.apply \
+  test_stdlib_setting_colour_enable__tput_fails_____@vary__@vary__does_not_disable_colours \
+  @parametrize_with_verbose
+
+test_stdlib_setting_colour_enable__tput_fails_____@vary__@vary__returns_status_code_127() {
   # shellcheck disable=SC2034
-  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="0"
+  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="${SILENT_FALLBACK_BOOLEAN}"
   local TERM=""
 
   _capture.rc stdlib.setting.colour.enable
@@ -85,27 +127,13 @@ test_stdlib_setting_colour_enable__tput_fails_____verbose_failure___returns_stat
   assert_rc "1"
 }
 
-test_stdlib_setting_colour_enable__tput_fails_____verbose_failure___does_not_set_call_colour_values() {
+@parametrize.apply \
+  test_stdlib_setting_colour_enable__tput_fails_____@vary__@vary__returns_status_code_127 \
+  @parametrize_with_verbose
+
+test_stdlib_setting_colour_enable__tput_succeeds__@vary__@vary__does_not_generate_error_message() {
   # shellcheck disable=SC2034
-  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="0"
-  local TERM=""
-  local colour
-
-  for colour in "${TEST_COLOURS[@]}"; do
-    printf -v "${colour}" "initial value"
-    assert_equals "${!colour}" "initial value"
-  done
-
-  stdlib.setting.colour.enable
-
-  for colour in "${TEST_COLOURS[@]}"; do
-    assert_equals "${!colour}" "initial value"
-  done
-}
-
-test_stdlib_setting_colour_enable__tput_succeeds__silent_fallback___does_not_generate_error_message() {
-  # shellcheck disable=SC2034
-  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="1"
+  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="${SILENT_FALLBACK_BOOLEAN}"
   local TERM="xterm"
 
   stdlib.setting.colour.enable
@@ -113,9 +141,14 @@ test_stdlib_setting_colour_enable__tput_succeeds__silent_fallback___does_not_gen
   stdlib.setting.colour.enable.__generate_error_message.mock.assert_not_called
 }
 
-test_stdlib_setting_colour_enable__tput_succeeds__silent_fallback___does_not_disable_colours() {
+@parametrize.apply \
+  test_stdlib_setting_colour_enable__tput_succeeds__@vary__@vary__does_not_generate_error_message \
+  @parametrize_with_verbose \
+  @parametrize_with_silent_
+
+test_stdlib_setting_colour_enable__tput_succeeds__@vary__@vary__does_not_disable_colours() {
   # shellcheck disable=SC2034
-  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="1"
+  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="${SILENT_FALLBACK_BOOLEAN}"
   local TERM="xterm"
 
   _capture.rc stdlib.setting.colour.enable
@@ -123,9 +156,14 @@ test_stdlib_setting_colour_enable__tput_succeeds__silent_fallback___does_not_dis
   stdlib.setting.colour.disable.mock.assert_not_called
 }
 
-test_stdlib_setting_colour_enable__tput_succeeds__silent_fallback___returns_status_code_0() {
+@parametrize.apply \
+  test_stdlib_setting_colour_enable__tput_succeeds__@vary__@vary__does_not_disable_colours \
+  @parametrize_with_verbose \
+  @parametrize_with_silent_
+
+test_stdlib_setting_colour_enable__tput_succeeds__@vary__@vary__returns_status_code_0() {
   # shellcheck disable=SC2034
-  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="1"
+  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="${SILENT_FALLBACK_BOOLEAN}"
   local TERM="xterm"
 
   _capture.rc stdlib.setting.colour.enable
@@ -133,9 +171,14 @@ test_stdlib_setting_colour_enable__tput_succeeds__silent_fallback___returns_stat
   assert_rc "0"
 }
 
-test_stdlib_setting_colour_enable__tput_succeeds__silent_fallback___sets_all_colour_variables_to_non_null_values() {
+@parametrize.apply \
+  test_stdlib_setting_colour_enable__tput_succeeds__@vary__@vary__returns_status_code_0 \
+  @parametrize_with_verbose \
+  @parametrize_with_silent_
+
+test_stdlib_setting_colour_enable__tput_succeeds__@vary__@vary__sets_all_colour_variables_to_non_null_values() {
   # shellcheck disable=SC2034
-  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="1"
+  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="${SILENT_FALLBACK_BOOLEAN}"
   local TERM="xterm"
   local colour
 
@@ -152,51 +195,7 @@ test_stdlib_setting_colour_enable__tput_succeeds__silent_fallback___sets_all_col
   done
 }
 
-test_stdlib_setting_colour_enable__tput_succeeds__verbose_failure___does_not_generate_error_message() {
-  # shellcheck disable=SC2034
-  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="0"
-  local TERM="xterm"
-
-  stdlib.setting.colour.enable
-
-  stdlib.setting.colour.enable.__generate_error_message.mock.assert_not_called
-}
-
-test_stdlib_setting_colour_enable__tput_succeeds__verbose_failure___does_not_disable_colours() {
-  # shellcheck disable=SC2034
-  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="1"
-  local TERM="xterm"
-
-  _capture.rc stdlib.setting.colour.enable
-
-  stdlib.setting.colour.disable.mock.assert_not_called
-}
-
-test_stdlib_setting_colour_enable__tput_succeeds__verbose_failure___returns_status_code_0() {
-  # shellcheck disable=SC2034
-  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="0"
-  local TERM="xterm"
-
-  _capture.rc stdlib.setting.colour.enable
-
-  assert_rc "0"
-}
-
-test_stdlib_setting_colour_enable__tput_succeeds__verbose_failure___sets_all_colour_variables_to_non_null_values() {
-  # shellcheck disable=SC2034
-  local STDLIB_COLOUR_SILENT_FALLBACK_BOOLEAN="0"
-  local TERM="xterm"
-  local colour
-
-  for colour in "${TEST_COLOURS[@]}"; do
-    printf -v "${colour}" "initial value"
-    assert_equals "${!colour}" "initial value"
-  done
-
-  stdlib.setting.colour.enable
-
-  for colour in "${TEST_COLOURS[@]}"; do
-    assert_not_equals "${!colour}" "initial value"
-    assert_not_null "${!colour}"
-  done
-}
+@parametrize.apply \
+  test_stdlib_setting_colour_enable__tput_succeeds__@vary__@vary__sets_all_colour_variables_to_non_null_values \
+  @parametrize_with_verbose \
+  @parametrize_with_silent_
