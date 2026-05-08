2ff4a516fa5c61d9e3b8022e5e387e82478dd9dc
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-05-08 08:37:03 -0400

    feat(VAR): add query/assertion for set vars

---
+++ src/var/query/tests/test_stdlib_var_query_is_set.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+setup() {
+  _TEST_SET_VAR="exists"
+}
+
+@parametrize_with_arg_combos() {
+  # $1: test function to parametrize
+
+  @parametrize \
+    "${1}" \
+    "TEST_ARGS_DEFINITION;TEST_EXPECTED_RC" \
+    "no_args__________returns_status_code_127;;127" \
+    "extra_arg________returns_status_code_127;_TEST_SET_VAR|extra_arg;127" \
+    "invalid_name_____returns_status_code_126;not a valid name;126" \
+    "not_set__________returns_status_code___1;_NOT_SET_VAR;1" \
+    "is_set___________returns_status_code___0;_TEST_SET_VAR;0"
+}
+
+# shellcheck disable=SC2153
+test_stdlib_var_query_is_set__@vary() {
+  local args=()
+
+  stdlib.array.make.from_string args "|" "${TEST_ARGS_DEFINITION}"
+
+  _capture.rc stdlib.var.query.is_set "${args[@]}" > /dev/null
+
+  assert_rc "${TEST_EXPECTED_RC}"
+}
+
+@parametrize_with_arg_combos \
+  test_stdlib_var_query_is_set__@vary
