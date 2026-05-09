ffe740a49ed2cddb2edf43a5bc0664f54828e83c
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-05-09 14:49:14 -0400

    feat(STRING): sensible colour selection fallback

--- src/setting/theme.sh
+++ src/setting/theme.sh
@@ -4,9 +4,14 @@
 
 builtin set -eo pipefail
 
+# shellcheck disable=SC2034
+STDLIB_COLOUR_NULL=""
+
 # @description Gets the name of a colour variable from the theme.
+#   * STDLIB_COLOUR_NULL string global: A default value for null or missing colours (default="").
 # @arg $1 string The name of the colour.
 # @exitcode 0 If the operation succeeded.
+# @set STDLIB_COLOUR_NULL string A default value for null or missing colours.
 # @stdout The name of the colour variable.
 # @stderr The error message if the operation fails.
 stdlib.setting.theme.get_colour() {
@@ -14,8 +19,10 @@ stdlib.setting.theme.get_colour() {
 
   theme_colour="STDLIB_COLOUR_${1}" # noqa
 
-  if [[ -z "${!theme_colour+set}" ]]; then
-    stdlib.logger.warning "$(stdlib.__message.get COLOUR_NOT_DEFINED "${1}")"
+  if ! stdlib.var.query.is_set "${theme_colour}"; then
+    # shellcheck disable=SC2034
+    STDLIB_COLOUR_NULL=""             # defaults STDLIB_COLOUR_NULL
+    theme_colour="STDLIB_COLOUR_NULL" #
   fi
 
   builtin echo "${theme_colour}"
