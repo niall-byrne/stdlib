ca48b8e9e4f9b38d3015d6cc1de8acc11d4da310
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-05-09 09:21:27 -0400

    internal global validation

--- src/var/assert/is.sh
+++ src/var/assert/is.sh
@@ -81,3 +81,32 @@ stdlib.var.assert.is_valid_with() {
 
   builtin return "${return_code}"
 }
+
+# @description Asserts a stdlib internally used variable's value is valid against a validation function.
+#   * STDLIB_VAR_VALIDATE_BY_NAME_BOOLEAN: A boolean value that controls whether the variable's name or value is passed to the validation function (default=0).
+#   * STDLIB_VAR_VALIDATE_DEFAULT_VAR: An optional variable name that can be used as a default source if the given variable name is unset (default="").
+# @arg $1 string The validation function to run.
+# @arg $2 string The name of the variable containing the value to perform validation on.
+# @exitcode 0 If the variable passes the validation function.
+# @exitcode 1 If the variable fails the validation check.
+# @exitcode 126 If an invalid argument has been provided.
+# @exitcode 127 If the wrong number of arguments were provided.
+# @stderr The error message if the assertion fails.
+# @internal
+stdlib.var.__assert.is_valid_stdlib_global_with() {
+  builtin local return_code=0
+
+  stdlib.var.query.is_valid_with "${@}" || return_code="$?"
+
+  case "${return_code}" in
+    0) ;; # KCOV_EXCLUDE_LINE
+    126 | 127)
+      stdlib.logger.error "$(stdlib.__message.get ARGUMENTS_INVALID)"
+      ;;
+    *)
+      stdlib.logger.error "$(stdlib.__message.get VAR_VALUE_INVALID "${2}")"
+      ;;
+  esac
+
+  builtin return "${return_code}"
+}
