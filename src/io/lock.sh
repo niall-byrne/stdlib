6b3b523966a2a5e6ff2f0d381aa7f0939a8049e9
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-05-09 15:47:46 -0400

    feat(IO): lock file permission controls

--- src/io/lock.sh
+++ src/io/lock.sh
@@ -6,14 +6,17 @@ builtin set -eo pipefail
 
 # shellcheck disable=SC2034
 {
+  STDLIB_LOCK_PERMISSION_OCTAL=""
   STDLIB_LOCK_QUIET_FAILURE_BOOLEAN=""
   STDLIB_LOCK_POLLING_INTERVAL=""
   STDLIB_LOCK_WAIT_SECONDS=""
+  STDLIB_LOCK_WORKSPACE_PERMISSION_OCTAL=""
 }
 
 builtin export STDLIB_LOCK_WORKSPACE=""
 
 # @description Acquires a named exclusive execution lock, or waits until able to do so.
+#   * STDLIB_LOCK_PERMISSION_OCTAL string keyword: An octal file system permission value for the created lock (default="0700").
 #   * STDLIB_LOCK_POLLING_INTERVAL string keyword: A decimal value for the number of seconds the process will wait before retrying lock acquisition (default="0.1").
 #   * STDLIB_LOCK_QUIET_FAILURE_BOOLEAN boolean keyword: A boolean to disable errors messages on a lock acquisition failure (default=0).
 #   * STDLIB_LOCK_WAIT_SECONDS integer keyword: An integer for the number of seconds the process will wait for the lock to become available.  To create an infinite wait, use a negative value. (default=30).
@@ -26,9 +29,11 @@ builtin export STDLIB_LOCK_WORKSPACE=""
 # @exitcode 126 If an invalid argument has been provided.
 # @exitcode 127 If the wrong number of arguments were provided.
 # @set STDLIB_LOCK_WORKSPACE string The name of a managed temporary directory which has been allocated for lock operations.
+# @set STDLIB_LOCK_WORKSPACE string The name of a managed temporary directory which has been allocated for lock operations.
 # @stderr The error message if the operation fails.
 stdlib.io.lock.acquire() {
   builtin local lock_name="${1}"
+  builtin local lock_permissions
   builtin local polling_interval
   builtin local quiet_acquisition_failure_boolean
   builtin local time_elapsed
@@ -38,10 +43,13 @@ stdlib.io.lock.acquire() {
   stdlib.fn.args.require "1" "0" "$@" || builtin return "$?"
   stdlib.var.query.is_valid_name "${lock_name}" || builtin return 126
 
+  stdlib.fn.keyword.consume lock_permissions STDLIB_LOCK_PERMISSION_OCTAL "0700"
   stdlib.fn.keyword.consume polling_interval STDLIB_LOCK_POLLING_INTERVAL "0.1"
   stdlib.fn.keyword.consume quiet_acquisition_failure_boolean STDLIB_LOCK_QUIET_FAILURE_BOOLEAN "0"
   stdlib.fn.keyword.consume wait_time STDLIB_LOCK_WAIT_SECONDS "30"
 
+  STDLIB_KW_SOURCE_VAR="lock_permissions" \
+    stdlib.fn.keyword.assert.is_valid_with stdlib.string.assert.is_octal_permission STDLIB_LOCK_PERMISSION_OCTAL || builtin return 125 # validates STDLIB_LOCK_PERMISSION_OCTAL
   STDLIB_KW_SOURCE_VAR="polling_interval" \
     stdlib.fn.keyword.assert.is_valid_with stdlib.string.assert.is_decimal_positive STDLIB_LOCK_POLLING_INTERVAL || builtin return 125 # validates STDLIB_LOCK_POLLING_INTERVAL
   STDLIB_KW_SOURCE_VAR="quiet_acquisition_failure_boolean" \
@@ -71,6 +79,8 @@ stdlib.io.lock.acquire() {
       builtin return 1
     fi
   done
+
+  "${_STDLIB_BINARY_CHMOD}" "${lock_permissions}" "${STDLIB_LOCK_WORKSPACE}/${lock_name}"
 
   builtin return 0
 }
@@ -102,10 +112,12 @@ stdlib.io.lock.release() {
 }
 
 # @description Runs a command with a named exclusive execution lock. A lock workspace is allocated as needed.
+#   * STDLIB_LOCK_PERMISSION_OCTAL string keyword: An octal file system permission value for the created lock (default="0700").
 #   * STDLIB_LOCK_POLLING_INTERVAL string keyword: A decimal value for the number of seconds the process will wait before retrying lock acquisition (default="0.1").
 #   * STDLIB_LOCK_QUIET_FAILURE_BOOLEAN boolean keyword: A boolean to disable errors messages on a lock acquisition failure (default=0).
 #   * STDLIB_LOCK_WAIT_SECONDS integer keyword: An integer for the number of seconds the process will wait for the lock to become available.  To create an infinite wait, use a negative value. (default=30).
 #   * STDLIB_LOCK_WORKSPACE string global: The name of a managed temporary directory which has been allocated for lock operations (default="").
+#   * STDLIB_LOCK_WORKSPACE_PERMISSION_OCTAL string keyword: An octal file system permission value for the created workspace folder (default="0700").
 # @arg $1 string A unique alpha-numeric name for this lock.
 # @arg $@ string The command or function and any arguments that will be executed with this execution lock.
 # @exitcode 0 If the lock was successfully acquired.
@@ -114,6 +126,7 @@ stdlib.io.lock.release() {
 # @exitcode 125 If an invalid keyword has been provided.
 # @exitcode 126 If an invalid argument has been provided.
 # @exitcode 127 If the wrong number of arguments were provided.
+# @set STDLIB_LOCK_WORKSPACE string The name of a managed temporary directory which has been allocated for lock operations.
 # @set STDLIB_LOCK_WORKSPACE string The name of a managed temporary directory which has been allocated for lock operations.
 # @stderr The error message if the operation fails.
 stdlib.io.lock.with() {
@@ -126,7 +139,7 @@ stdlib.io.lock.with() {
 
   stdlib.io.lock.workspace_allocate || builtin return "$?" # validates STDLIB_LOCK_WORKSPACE
 
-  stdlib.io.lock.acquire "${lock_name}" || builtin return "$?" # validates STDLIB_LOCK_POLLING_INTERVAL,STDLIB_LOCK_QUIET_FAILURE_BOOLEAN,STDLIB_LOCK_WAIT_SECONDS
+  stdlib.io.lock.acquire "${lock_name}" || builtin return "$?" # validates STDLIB_LOCK_PERMISSION_OCTAL,STDLIB_LOCK_POLLING_INTERVAL,STDLIB_LOCK_QUIET_FAILURE_BOOLEAN,STDLIB_LOCK_WAIT_SECONDS,STDLIB_LOCK_WORKSPACE_PERMISSION_OCTAL
 
   "${@}" && exit_code="$?" || exit_code="$?"
 
@@ -138,19 +151,27 @@ stdlib.io.lock.with() {
 # @description Creates a temporary folder dedicated for execution locking, and handles it's clean up.
 #   * STDLIB_HANDLER_EXIT_FN_ARRAY array global: An array containing a list of functions that are run on an exit call (default=()).
 #   * STDLIB_LOCK_WORKSPACE string global: The name of a managed temporary directory which has been allocated for lock operations (default="").
+#   * STDLIB_LOCK_WORKSPACE_PERMISSION_OCTAL string keyword: An octal file system permission value for the created workspace folder (default="0700").
 # @noargs
 # @exitcode 0 If the workspace was successfully allocated.
 # @exitcode 1 If the workspace could not be allocated.
 # @exitcode 123 If a variable reserved for use by the BASH stdlib has been assigned an invalid value.
+# @exitcode 125 If an invalid keyword has been provided.
 # @exitcode 127 If the wrong number of arguments were provided.
 # @set STDLIB_HANDLER_EXIT_FN_ARRAY array An array containing a list of functions that are run on an exit call.
 # @set STDLIB_LOCK_WORKSPACE string The name of a managed temporary directory which has been allocated for lock operations.
 # @stderr The error message if the operation fails.
 # shellcheck disable=SC2120
 stdlib.io.lock.workspace_allocate() {
+  builtin local lock_workspace_permissions
   builtin local successful_allocation_boolean=1
 
   stdlib.fn.args.require "0" "0" "$@" || builtin return "$?"
+
+  stdlib.fn.keyword.consume lock_workspace_permissions STDLIB_LOCK_WORKSPACE_PERMISSION_OCTAL "0700"
+
+  STDLIB_KW_SOURCE_VAR="lock_workspace_permissions" \
+    stdlib.fn.keyword.assert.is_valid_with stdlib.string.assert.is_octal_permission STDLIB_LOCK_WORKSPACE_PERMISSION_OCTAL || builtin return 125 # validates STDLIB_LOCK_WORKSPACE_PERMISSION_OCTAL
 
   if stdlib.io.lock.__workspace_is_valid "${STDLIB_LOCK_WORKSPACE}"; then # validates STDLIB_LOCK_WORKSPACE
     builtin return 0
@@ -169,7 +190,9 @@ stdlib.io.lock.workspace_allocate() {
     builtin return 1
   fi
 
-  STDLIB_HANDLER_EXIT_FN_ARRAY+=("stdlib.io.lock.__workspace_cleanup")
+  "${_STDLIB_BINARY_CHMOD}" "${lock_workspace_permissions}" "${STDLIB_LOCK_WORKSPACE}"
+
+  STDLIB_HANDLER_EXIT_FN_ARRAY+=("stdlib.io.lock.__workspace_cleanup") # validates STDLIB_HANDLER_EXIT_FN_ARRAY
 }
 
 # @description Cleans up the temporary folder dedicated for execution locking.
