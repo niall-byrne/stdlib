cc612252208863c5b468dfa0fb1f34f74bf1765d
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-05-11 10:52:17 -0400

    feat(VAR): add assertion to validate reserved vars

--- src/message.sh
+++ src/message.sh
@@ -278,6 +278,10 @@ stdlib.__message.get() {
       required_options=1
       message="$(stdlib.__gettext "The variable '\${option1}' has an invalid value!")"
       ;;
+    VAR_VALUE_INVALID_RESERVED_DETAIL)
+      required_options=1
+      message="$(stdlib.__gettext "The variable '\${option1}' is reserved for internal use by the BASH stdlib and has been assigned an invalid value!")"
+      ;;
     VAR_VALUE_NOT_EMPTY)
       required_options=1
       message="$(stdlib.__gettext "The variable '\${option1}' has been assigned a non-empty value!")"
