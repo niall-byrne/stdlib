96f3fc11da66b8f7a185578213ccd17a15578e5b
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-05-11 14:15:38 -0400

    feat(TRAPS): global variable validation

--- dist/stdlib.sh
+++ dist/stdlib.sh
@@ -3920,6 +3920,10 @@ stdlib.string.wrap_pipe ()
 
 stdlib.trap.__register_default_handlers ()
 {
+    stdlib.var.reserved.assert.__is_valid_with stdlib.array.assert.is_array STDLIB_CLEANUP_FN_TARGETS_ARRAY name || builtin return 123;
+    stdlib.var.reserved.assert.__is_valid_with stdlib.array.assert.is_array STDLIB_HANDLER_ERR_FN_ARRAY name || builtin return 123;
+    stdlib.var.reserved.assert.__is_valid_with stdlib.array.assert.is_array STDLIB_HANDLER_EXIT_FN_ARRAY name || builtin return 123;
+    stdlib.var.reserved.assert.__is_valid_with stdlib.string.assert.is_boolean STDLIB_TRACEBACK_DISABLE_BOOLEAN || builtin return 123;
     stdlib.trap.create.handler "stdlib.trap.handler.err.fn" STDLIB_HANDLER_ERR_FN_ARRAY;
     stdlib.trap.create.handler "stdlib.trap.handler.exit.fn" STDLIB_HANDLER_EXIT_FN_ARRAY;
     stdlib.trap.create.cleanup_fn "stdlib.trap.fn.cleanup_on_exit" STDLIB_CLEANUP_FN_TARGETS_ARRAY;
@@ -3946,6 +3950,8 @@ ${1}() {
 
   [[ "\${#@}" -eq 0 ]] || builtin return 127
 
+  stdlib.var.global.assert.is_valid_with stdlib.array.assert.is_array "${2}" name || builtin return 126
+
   for clean_up_path in "\${${2}[@]}"; do
     if stdlib.io.path.query.is_exists "\${clean_up_path}"; then
       "${_STDLIB_BINARY_RM}" "${rm_flags}" "\${clean_up_path}"
@@ -3968,7 +3974,10 @@ ${1}() {
 
   [[ "\${#@}" -eq 0 ]] || builtin return 127
 
+  stdlib.var.global.assert.is_valid_with stdlib.array.assert.is_array "${2}" name || builtin return 126
+
   for trap_handler_fn in "\${${2}[@]}"; do
+    stdlib.fn.assert.is_fn "\${trap_handler_fn}" || builtin return 126
     "\${trap_handler_fn}"
   done
 }
@@ -3978,6 +3987,8 @@ ${1}.register() {
 
   stdlib.fn.args.require "1" "0" "\${@}" || builtin return "\$?"
   stdlib.fn.assert.is_fn "\${1}" || builtin return 126
+
+  stdlib.var.global.assert.is_valid_with stdlib.array.assert.is_array "${2}" name || builtin return 126
 
   ${2}+=("\${1}")
 }
@@ -3990,6 +4001,7 @@ stdlib.trap.fn.cleanup_on_exit ()
 {
     builtin local clean_up_path;
     [[ "${#@}" -eq 0 ]] || builtin return 127;
+    stdlib.var.global.assert.is_valid_with stdlib.array.assert.is_array "STDLIB_CLEANUP_FN_TARGETS_ARRAY" name || builtin return 126;
     for clean_up_path in "${STDLIB_CLEANUP_FN_TARGETS_ARRAY[@]}";
     do
         if stdlib.io.path.query.is_exists "${clean_up_path}"; then
@@ -4002,8 +4014,10 @@ stdlib.trap.handler.err.fn ()
 {
     builtin local trap_handler_fn;
     [[ "${#@}" -eq 0 ]] || builtin return 127;
+    stdlib.var.global.assert.is_valid_with stdlib.array.assert.is_array "STDLIB_HANDLER_ERR_FN_ARRAY" name || builtin return 126;
     for trap_handler_fn in "${STDLIB_HANDLER_ERR_FN_ARRAY[@]}";
     do
+        stdlib.fn.assert.is_fn "${trap_handler_fn}" || builtin return 126;
         "${trap_handler_fn}";
     done
 }
@@ -4012,6 +4026,7 @@ stdlib.trap.handler.err.fn.register ()
 {
     stdlib.fn.args.require "1" "0" "${@}" || builtin return "$?";
     stdlib.fn.assert.is_fn "${1}" || builtin return 126;
+    stdlib.var.global.assert.is_valid_with stdlib.array.assert.is_array "STDLIB_HANDLER_ERR_FN_ARRAY" name || builtin return 126;
     STDLIB_HANDLER_ERR_FN_ARRAY+=("${1}")
 }
 
@@ -4019,8 +4034,10 @@ stdlib.trap.handler.exit.fn ()
 {
     builtin local trap_handler_fn;
     [[ "${#@}" -eq 0 ]] || builtin return 127;
+    stdlib.var.global.assert.is_valid_with stdlib.array.assert.is_array "STDLIB_HANDLER_EXIT_FN_ARRAY" name || builtin return 126;
     for trap_handler_fn in "${STDLIB_HANDLER_EXIT_FN_ARRAY[@]}";
     do
+        stdlib.fn.assert.is_fn "${trap_handler_fn}" || builtin return 126;
         "${trap_handler_fn}";
     done
 }
@@ -4029,6 +4046,7 @@ stdlib.trap.handler.exit.fn.register ()
 {
     stdlib.fn.args.require "1" "0" "${@}" || builtin return "$?";
     stdlib.fn.assert.is_fn "${1}" || builtin return 126;
+    stdlib.var.global.assert.is_valid_with stdlib.array.assert.is_array "STDLIB_HANDLER_EXIT_FN_ARRAY" name || builtin return 126;
     STDLIB_HANDLER_EXIT_FN_ARRAY+=("${1}")
 }
 
@@ -4229,7 +4247,7 @@ builtin set -e
 
 # this snippet is included by the build script:
 # src/trap/register.snippet
-stdlib.trap.__register_default_handlers
+stdlib.trap.__register_default_handlers || builtin return "$?"
 
 stdlib.__builtin.overridable trap stdlib.trap.handler.err.fn ERR
 stdlib.__builtin.overridable trap stdlib.trap.handler.exit.fn EXIT
