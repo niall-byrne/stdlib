90762b62d322f3643db72ba0e9930b8dc0435751
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-05-11 10:52:17 -0400

    feat(VAR): add assertion to validate reserved vars

--- dist/stdlib.sh
+++ dist/stdlib.sh
@@ -415,6 +415,10 @@ stdlib.__message.get ()
             required_options=1;
             message="$(stdlib.__gettext "The variable '\${option1}' has an invalid value!")"
         ;;
+        VAR_VALUE_INVALID_RESERVED_DETAIL)
+            required_options=1;
+            message="$(stdlib.__gettext "The variable '\${option1}' is reserved for internal use by the BASH stdlib and has been assigned an invalid value!")"
+        ;;
         VAR_VALUE_NOT_EMPTY)
             required_options=1;
             message="$(stdlib.__gettext "The variable '\${option1}' has been assigned a non-empty value!")"
@@ -4076,6 +4080,24 @@ stdlib.var.query.is_valid_with ()
     else
         "${1}" "${!validation_source}" || return_code="$?";
     fi;
+    builtin return "${return_code}"
+}
+
+stdlib.var.reserved.assert.__is_valid_with ()
+{
+    builtin local return_code=0;
+    stdlib.var.query.is_valid_with "${@}" || return_code="$?";
+    case "${return_code}" in
+        0)
+
+        ;;
+        126 | 127)
+            stdlib.logger.error "$(stdlib.__message.get ARGUMENTS_INVALID)"
+        ;;
+        *)
+            stdlib.logger.error "$(stdlib.__message.get VAR_VALUE_INVALID_RESERVED_DETAIL "${2}")"
+        ;;
+    esac;
     builtin return "${return_code}"
 }
 
