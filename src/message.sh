fe4709166aebcf3c4e631844bef9b7bf84515462
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-05-20 13:14:19 -0400

    feat(VAR): add assertion to validate global vars

--- src/message.sh
+++ src/message.sh
@@ -278,6 +278,10 @@ stdlib.__message.get() {
       required_options=1
       message="$(stdlib.__gettext "The variable '\${option1}' has an invalid value!")"
       ;;
+    VAR_VALUE_INVALID_GLOBAL_DETAIL)
+      required_options=1
+      message="$(stdlib.__gettext "The global variable '\${option1}' has been assigned an invalid value!")"
+      ;;
     VAR_VALUE_INVALID_RESERVED_DETAIL)
       required_options=1
       message="$(stdlib.__gettext "The variable '\${option1}' is reserved for internal use by the BASH stdlib and has been assigned an invalid value!")"
