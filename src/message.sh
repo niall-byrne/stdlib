dd91f860e41a45d55d3e812fd13c747be9dd8d37
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-05-20 11:03:42 -0400

    feat(VAR): add query/assertion for empty vars

--- src/message.sh
+++ src/message.sh
@@ -270,6 +270,10 @@ stdlib.__message.get() {
       required_options=1
       message="$(stdlib.__gettext "The value '\${option1}' is not a valid variable name!")"
       ;;
+    VAR_VALUE_NOT_EMPTY)
+      required_options=1
+      message="$(stdlib.__gettext "The variable '\${option1}' has been assigned a non-empty value!")"
+      ;;
     "")
       required_options=0
       return_status=126
