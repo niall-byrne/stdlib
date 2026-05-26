8b0e23ffbe6d736467cecbe6023d341600477c80
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-05-26 10:55:36 -0400

    feat(TESTING): error logging assertion

---
+++ src/testing/assertion/logger.sh
@@ -0,0 +1,145 @@
+#!/bin/bash
+
+# stdlib logger extensions to bash_unit assertions
+
+builtin set -eo pipefail
+
+# @description Asserts that the stdlib.logger.error function was mocked and called with specific arguments, or alternatively, not called at all.
+#   * STDLIB_LOGGING_MESSAGE_PREFIX: If the mock has this keyword set, then the value of this keyword is used when matching (default="").
+# @arg $@ array A list of message strings the logger is expected to have been called with.  An empty list asserts that the logger was not called.
+# @exitcode 0 If the assertion passed.
+# @exitcode 1 If the assertion fails, or if the logger has not been mocked.
+# @stderr The error message if the assertion fails.
+assert_logger_error_matches() {
+  builtin local -a message_args
+
+  assert_is_mock stdlib.logger.error || builtin return "$?"
+
+  message_args=("${@}")
+  _testing.__protected stdlib.array.mutate.filter "$(_testing.__protected_name stdlib.string.query.not_empty)" message_args
+
+  if ! _testing.__protected stdlib.array.query.is_empty message_args; then
+    if _testing.__protected stdlib.string.query.has_substring STDLIB_LOGGING_MESSAGE_PREFIX "$(stdlib.logger.error.mock.get.keywords)"; then
+      _testing.__protected stdlib.array.mutate.format "1(%s) STDLIB_LOGGING_MESSAGE_PREFIX(${STDLIB_LOGGING_MESSAGE_PREFIX})" message_args
+    else
+      _testing.__protected stdlib.array.mutate.format "1(%s)" message_args
+    fi
+
+    stdlib.logger.error.mock.assert_calls_are "${message_args[@]}"
+  else
+
+    stdlib.logger.error.mock.assert_not_called
+  fi
+}
+
+# @description Asserts that the stdlib.logger.info function was mocked and called with specific arguments, or alternatively, not called at all.
+#   * STDLIB_LOGGING_MESSAGE_PREFIX: If the mock has this keyword set, then the value of this keyword is used when matching (default="").
+# @arg $@ array A list of message strings the logger is expected to have been called with.  An empty list asserts that the logger was not called.
+# @exitcode 0 If the assertion passed.
+# @exitcode 1 If the assertion fails, or if the logger has not been mocked.
+# @stderr The error message if the assertion fails.
+assert_logger_info_matches() {
+  builtin local -a message_args
+
+  assert_is_mock stdlib.logger.info || builtin return "$?" # noqa
+
+  message_args=("${@}")
+  _testing.__protected stdlib.array.mutate.filter "$(_testing.__protected_name stdlib.string.query.not_empty)" message_args
+
+  if ! _testing.__protected stdlib.array.query.is_empty message_args; then
+    if _testing.__protected stdlib.string.query.has_substring STDLIB_LOGGING_MESSAGE_PREFIX "$(stdlib.logger.info.mock.get.keywords)"; then
+      _testing.__protected stdlib.array.mutate.format "1(%s) STDLIB_LOGGING_MESSAGE_PREFIX(${STDLIB_LOGGING_MESSAGE_PREFIX})" message_args
+    else
+      _testing.__protected stdlib.array.mutate.format "1(%s)" message_args
+    fi
+
+    stdlib.logger.info.mock.assert_calls_are "${message_args[@]}"
+  else
+
+    stdlib.logger.info.mock.assert_not_called
+  fi
+}
+
+# @description Asserts that the stdlib.logger.notice function was mocked and called with specific arguments, or alternatively, not called at all.
+#   * STDLIB_LOGGING_MESSAGE_PREFIX: If the mock has this keyword set, then the value of this keyword is used when matching (default="").
+# @arg $@ array A list of message strings the logger is expected to have been called with.  An empty list asserts that the logger was not called.
+# @exitcode 0 If the assertion passed.
+# @exitcode 1 If the assertion fails, or if the logger has not been mocked.
+# @stderr The error message if the assertion fails.
+assert_logger_notice_matches() {
+  builtin local -a message_args
+
+  assert_is_mock stdlib.logger.notice || builtin return "$?" # noqa
+
+  message_args=("${@}")
+  _testing.__protected stdlib.array.mutate.filter "$(_testing.__protected_name stdlib.string.query.not_empty)" message_args
+
+  if ! _testing.__protected stdlib.array.query.is_empty message_args; then
+    if _testing.__protected stdlib.string.query.has_substring STDLIB_LOGGING_MESSAGE_PREFIX "$(stdlib.logger.notice.mock.get.keywords)"; then
+      _testing.__protected stdlib.array.mutate.format "1(%s) STDLIB_LOGGING_MESSAGE_PREFIX(${STDLIB_LOGGING_MESSAGE_PREFIX})" message_args
+    else
+      _testing.__protected stdlib.array.mutate.format "1(%s)" message_args
+    fi
+
+    stdlib.logger.notice.mock.assert_calls_are "${message_args[@]}"
+  else
+
+    stdlib.logger.notice.mock.assert_not_called
+  fi
+}
+
+# @description Asserts that the stdlib.logger.success function was mocked and called with specific arguments, or alternatively, not called at all.
+#   * STDLIB_LOGGING_MESSAGE_PREFIX: If the mock has this keyword set, then the value of this keyword is used when matching (default="").
+# @arg $@ array A list of message strings the logger is expected to have been called with.  An empty list asserts that the logger was not called.
+# @exitcode 0 If the assertion passed.
+# @exitcode 1 If the assertion fails, or if the logger has not been mocked.
+# @stderr The error message if the assertion fails.
+assert_logger_success_matches() {
+  builtin local -a message_args
+
+  assert_is_mock stdlib.logger.success || builtin return "$?" # noqa
+
+  message_args=("${@}")
+  _testing.__protected stdlib.array.mutate.filter stdlib.string.query.not_empty message_args
+
+  if ! _testing.__protected stdlib.array.query.is_empty message_args; then
+    if _testing.__protected stdlib.string.query.has_substring STDLIB_LOGGING_MESSAGE_PREFIX "$(stdlib.logger.success.mock.get.keywords)"; then
+      _testing.__protected stdlib.array.mutate.format "1(%s) STDLIB_LOGGING_MESSAGE_PREFIX(${STDLIB_LOGGING_MESSAGE_PREFIX})" message_args
+    else
+      _testing.__protected stdlib.array.mutate.format "1(%s)" message_args
+    fi
+
+    stdlib.logger.success.mock.assert_calls_are "${message_args[@]}"
+  else
+
+    stdlib.logger.success.mock.assert_not_called
+  fi
+}
+
+# @description Asserts that the stdlib.logger.warning function was mocked and called with specific arguments, or alternatively, not called at all.
+#   * STDLIB_LOGGING_MESSAGE_PREFIX: If the mock has this keyword set, then the value of this keyword is used when matching (default="").
+# @arg $@ array A list of message strings the logger is expected to have been called with.  An empty list asserts that the logger was not called.
+# @exitcode 0 If the assertion passed.
+# @exitcode 1 If the assertion fails, or if the logger has not been mocked.
+# @stderr The error message if the assertion fails.
+assert_logger_warning_matches() {
+  builtin local -a message_args
+
+  assert_is_mock stdlib.logger.warning || builtin return "$?"
+
+  message_args=("${@}")
+  _testing.__protected stdlib.array.mutate.filter "$(_testing.__protected_name stdlib.string.query.not_empty)" message_args
+
+  if ! _testing.__protected stdlib.array.query.is_empty message_args; then
+    if _testing.__protected stdlib.string.query.has_substring STDLIB_LOGGING_MESSAGE_PREFIX "$(stdlib.logger.warning.mock.get.keywords)"; then
+      _testing.__protected stdlib.array.mutate.format "1(%s) STDLIB_LOGGING_MESSAGE_PREFIX(${STDLIB_LOGGING_MESSAGE_PREFIX})" message_args
+    else
+      _testing.__protected stdlib.array.mutate.format "1(%s)" message_args
+    fi
+
+    stdlib.logger.warning.mock.assert_calls_are "${message_args[@]}"
+  else
+
+    stdlib.logger.warning.mock.assert_not_called
+  fi
+}
