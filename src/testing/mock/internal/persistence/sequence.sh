92ac188eed4ec8944317bdc7cda3e272657abf13
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-05-27 10:20:40 -0400

    mock internal changes

--- src/testing/mock/internal/persistence/sequence.sh
+++ src/testing/mock/internal/persistence/sequence.sh
@@ -5,10 +5,14 @@
 builtin set -eo pipefail
 
 __STDLIB_TESTING_MOCK_SEQUENCE_LOCK_NAME="__stdlib_testing_internal__mock_sequence_lock"
+__STDLIB_TESTING_MOCK_SEQUENCE_FILENAME=""
 
 # @description Clears the persisted sequence of mock calls.
+#   * __STDLIB_TESTING_MOCK_SEQUENCE_ARRAY: An array containing the sequence of mock class (default=()).
+#   * __STDLIB_TESTING_MOCK_SEQUENCE_FILENAME: The filename containing the persisted sequence of mock calls (default="").
 # @noargs
 # @exitcode 0 If the sequence was cleared.
+# @set __STDLIB_TESTING_MOCK_SEQUENCE_ARRAY array The sequence of mock calls.
 # @internal
 _mock.__internal.persistence.sequence.clear() {
   __STDLIB_TESTING_MOCK_SEQUENCE_ARRAY=()
@@ -16,19 +20,23 @@ _mock.__internal.persistence.sequence.clear() {
 }
 
 # @description Initializes the persistence file for the mock call sequence.
+#   * __STDLIB_TESTING_MOCK_SEQUENCE_ARRAY: An array containing the sequence of mock class (default=()).
+#   * __STDLIB_TESTING_MOCK_REGISTRY_FOLDER: The folder where mock related tracking data is stored during test execution (default="").
 # @noargs
 # @exitcode 0 If the sequence persistence file was initialized.
 # @internal
 _mock.__internal.persistence.sequence.initialize() {
   if [[ -z "${__STDLIB_TESTING_MOCK_SEQUENCE_FILENAME}" ]]; then
-    __STDLIB_TESTING_MOCK_SEQUENCE_FILENAME="$("${_STDLIB_BINARY_MKTEMP}" -p "${__STDLIB_TESTING_MOCK_REGISTRY_FILENAME}")"
+    __STDLIB_TESTING_MOCK_SEQUENCE_FILENAME="$("${_STDLIB_BINARY_MKTEMP}" -p "${__STDLIB_TESTING_MOCK_REGISTRY_FOLDER}")"
     _mock.__internal.persistence.sequence.update
   fi
 }
 
 # @description Retrieves the persisted sequence of mock calls.
+#   * __STDLIB_TESTING_MOCK_SEQUENCE_FILENAME: The filename containing the persisted sequence of mock calls (default=""). 
 # @noargs
 # @exitcode 0 If the sequence was retrieved.
+# @set __STDLIB_TESTING_MOCK_SEQUENCE_ARRAY array The sequence of mock calls.
 # @internal
 _mock.__internal.persistence.sequence.retrieve() {
   builtin local -a __STDLIB_TESTING_MOCK_SEQUENCE_ARRAY_PERSISTED_ARRAY
@@ -38,6 +46,8 @@ _mock.__internal.persistence.sequence.retrieve() {
 }
 
 # @description Updates the persistence file with the current sequence of mock calls.
+#   * __STDLIB_TESTING_MOCK_SEQUENCE_ARRAY: An array containing the sequence of mock class (default=()).
+#   * __STDLIB_TESTING_MOCK_SEQUENCE_FILENAME: The filename containing the persisted sequence of mock calls (default=""). 
 # @noargs
 # @exitcode 0 If the sequence persistence file was updated.
 # @internal
--- src/testing/mock/internal/persistence/tests/registry/test_stdlib_testing_mock_internal_persistence_registry_cleanup.sh
+++ src/testing/mock/internal/persistence/tests/registry/test_stdlib_testing_mock_internal_persistence_registry_cleanup.sh
@@ -5,14 +5,14 @@ setup() {
 }
 
 test_stdlib_testing_mock_internal_persistence_registry_cleanup__registry_exists__________calls_rm_as_expected() {
-  _STDLIB_BINARY_RM="rm" __STDLIB_TESTING_MOCK_REGISTRY_FILENAME="test_registry" \
+  _STDLIB_BINARY_RM="rm" __STDLIB_TESTING_MOCK_REGISTRY_FOLDER="test_registry" \
     _mock.__internal.persistence.registry.cleanup
 
   rm.mock.assert_called_once_with "1(-rf) 2(test_registry)"
 }
 
 test_stdlib_testing_mock_internal_persistence_registry_cleanup__registry_does_not_exist__does_not_call_rm() {
-  _STDLIB_BINARY_RM="rm" __STDLIB_TESTING_MOCK_REGISTRY_FILENAME="" \
+  _STDLIB_BINARY_RM="rm" __STDLIB_TESTING_MOCK_REGISTRY_FOLDER="" \
     _mock.__internal.persistence.registry.cleanup
 
   rm.mock.assert_not_called
