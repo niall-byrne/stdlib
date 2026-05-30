884ab814b5bea80b8722b5785ffd0ec187c25de0
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-05-30 13:40:15 -0400

    feat(FN): allow disabling null args check

--- src/fn/args.sh
+++ src/fn/args.sh
@@ -6,10 +6,12 @@ builtin set -eo pipefail
 
 STDLIB_ARGS_CALLER_FN_NAME=""
 STDLIB_ARGS_NULL_SAFE_ARRAY=()
+STDLIB_ARGS_NULL_SAFE_ALL_BOOLEAN="0"
 
 # @description Validates the presence and number of arguments for a function.
 #   * STDLIB_ARGS_CALLER_FN_NAME: A string presented as the name of the calling function in logging messages (default="${FUNCNAME[1]}").
 #   * STDLIB_ARGS_NULL_SAFE_ARRAY: An array of argument indexes that are null safe, meaning they can be empty values (default=()).
+#   * STDLIB_ARGS_NULL_SAFE_ALL_BOOLEAN: A boolean that indicates all arguments are null safe, meaning they can be empty values (default="0").
 # @arg $1 integer The number of required arguments.
 # @arg $2 integer The number of optional arguments.
 # @arg $@ array The list of argument values to check.
@@ -29,6 +31,7 @@ stdlib.fn.args.require() {
   stdlib.string.assert.is_digit "${args_required_count}" || builtin return 126
   stdlib.string.assert.is_digit "${args_optional_count}" || builtin return 126
   stdlib.array.assert.is_array STDLIB_ARGS_NULL_SAFE_ARRAY || builtin return 126
+  stdlib.string.assert.is_boolean "${STDLIB_ARGS_NULL_SAFE_ALL_BOOLEAN}" || builtin return 126
 
   # shellcheck disable=SC2034
   args_null_safe_array=("${STDLIB_ARGS_NULL_SAFE_ARRAY[@]}")
@@ -39,6 +42,10 @@ stdlib.fn.args.require() {
     stdlib.logger.error "$(stdlib.__message.get ARGUMENT_REQUIREMENTS_VIOLATION "${args_required_count}" "${args_optional_count}")"
     stdlib.logger.error "$(stdlib.__message.get ARGUMENT_REQUIREMENTS_VIOLATION_DETAIL "${#@}")"
     builtin return 127
+  fi
+
+  if [[ "${STDLIB_ARGS_NULL_SAFE_ALL_BOOLEAN}" == "1" ]]; then
+    builtin return 0
   fi
 
   for ((arg_index = 1; arg_index <= "${#@}"; arg_index++)); do
