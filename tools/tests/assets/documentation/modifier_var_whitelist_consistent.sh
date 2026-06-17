d26b892e321980f0d48e3aa9aceed6bfdbab949c
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-06-17 14:33:46 -0400

    refactor(TOOLS): reorganize test assets/naming

--- tools/tests/assets/documentation/modifier_var_whitelist_consistent.sh
+++ tools/tests/assets/documentation/modifier_var_whitelist_consistent.sh
@@ -7,3 +7,11 @@
 stdlib.whitelist_fn1() {
   :
 }
+
+# @description Whitelist File 2.
+# @noargs
+# @exitcode 0 If successful.
+# @set TEST_RC string Description 2.
+stdlib.whitelist_fn2() {
+  :
+}
