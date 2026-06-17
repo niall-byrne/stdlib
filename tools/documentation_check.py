27230ff14060da30a2f151dbb3aadd4abf900a49
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-06-17 14:52:46 -0400

    feat(TOOLS): ignore stdout/stderr triggers in comments

--- tools/documentation_check.py
+++ tools/documentation_check.py
@@ -756,6 +756,8 @@ class MissingOutputTagsRule(Rule):
         is_stdout: bool = False,
     ) -> bool:
         for line in body:
+            if line.strip().startswith("#"):
+                continue
             if any((re.search(t + r"([\s\)]+|$)", line)) for t in triggers):
                 if is_stdout and "builtin echo" in line:
                     if (
