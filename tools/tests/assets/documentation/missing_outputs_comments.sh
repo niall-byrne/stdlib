f8e085685dc83765666ef6185689d523973937a1
Niall Byrne <9848926+niall-byrne@users.noreply.github.com>
2026-06-17 14:52:46 -0400

    feat(TOOLS): ignore stdout/stderr triggers in comments

---
+++ tools/tests/assets/documentation/missing_outputs_comments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# @description Missing stderr and stdout tags.
+# @arg $1 string An argument.
+# @exitcode 0 If the operation succeeded.
+stdlib.missing_outputs_comments() {
+  # builtin echo "stdout"
+  # builtin echo "stderr" >&2
+  :
+}
